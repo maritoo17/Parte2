91e6465aae53d48c83d131afd69245dcce2ad120
maritoo17 <msancgim@myuax.com>
2024-05-17 18:58:49 +0200

    8

---
+++ src/main/java/Bacteria.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Bacteria {
+    private int x;
+    private int y;
+    private int comidaConsumida;
+
+    public Bacteria(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.comidaConsumida = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getComidaConsumida() {
+        return comidaConsumida;
+    }
+
+    public void comer(int cantidad) {
+        comidaConsumida += cantidad;
+    }
+
+    public void mover(int nuevoX, int nuevoY) {
+        this.x = nuevoX;
+        this.y = nuevoY;
+    }
+}
