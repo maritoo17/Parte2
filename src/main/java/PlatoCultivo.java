74abc90aeb83f8cb8c5286f3e951da8915e070e2
maritoo17 <msancgim@myuax.com>
2024-05-17 19:00:41 +0200

    9

---
+++ src/main/java/PlatoCultivo.java
@@ -0,0 +1,25 @@
+public class PlatoCultivo {
+    private final int tamaño = 20;
+    private Celda[][] celdas;
+
+    public PlatoCultivo() {
+        celdas = new Celda[tamaño][tamaño];
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
+                celdas[i][j] = new Celda();
+            }
+        }
+    }
+
+    public Celda[][] getCeldas() {
+        return celdas;
+    }
+
+    public Celda getCelda(int x, int y) {
+        return celdas[x][y];
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+}
