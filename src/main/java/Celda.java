c7bd1bb7e7baf2b499f538ced069ccf618766988
maritoo17 <msancgim@myuax.com>
2024-05-17 18:58:05 +0200

    7

---
+++ src/main/java/Celda.java
@@ -0,0 +1,25 @@
+public class Celda {
+    private int bacterias;
+    private int comida;
+
+    public Celda() {
+        this.bacterias = 0;
+        this.comida = 0;
+    }
+
+    public int getBacterias() {
+        return bacterias;
+    }
+
+    public void setBacterias(int bacterias) {
+        this.bacterias = bacterias;
+    }
+
+    public int getComida() {
+        return comida;
+    }
+
+    public void setComida(int comida) {
+        this.comida = comida;
+    }
+}
